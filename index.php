b5ca5bf1258a406c8aca54f9691ed4afc51fed04
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-05 12:47:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 
@@ -40,7 +39,7 @@ if ($result->num_rows > 0) {
   echo "<table><tr><th>School ID</th><th>School Name</th></tr>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo <a href="test.php"><"<tr><td>".$row["school_id"].>"</td><td>".$row["school_name"]."</td></tr>";
+    echo "<tr><a href=test.php><td>".$row["school_id"].>"</td><td>".$row["school_name"]."</td></tr>";
   }
   echo "</table>";
 } else {
