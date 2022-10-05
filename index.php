7500f791f2451c30a0734743ddea3c2a09c3a9d6
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-04 21:37:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,30 @@
 <body>
 
 <?php
-$server = "165.227.18.177";
+$servername = "165.227.18.177";
 $username = "alqadiou";
 $password = "1[L0B2U1kkqXh*";
 $dbname = "myDB";
 
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+  
+  $sql = "SELECT * FROM classroom"
+    $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>Room Number</th><th>Room Building</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["room_num"]."</td><td>".$row["room_building"].;
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
 
 </body>
