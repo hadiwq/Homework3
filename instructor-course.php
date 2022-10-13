05cea656ea33c98e6f5a6bc2ec9ff801192454a6
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-12 21:38:45 -0500

    Update instructor-course.php

--- instructor-course.php
+++ instructor-course.php
@@ -58,8 +58,3 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-</tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
