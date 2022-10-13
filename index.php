39e6f993300c54a04fbec82a6d2daed8dd9ec400
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-12 19:47:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Home page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
@@ -12,16 +12,17 @@
 
 
 
-<head>
-<style>
-table, th, td {
-    border: 1px solid black;
-}
-</style>
-</head>
 <body>
-
-<?php
+    <h1></h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>School ID</th>
+      <th>School Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
 $servername = "165.227.18.177";
 $username = "alqadiou";
 $password = "1[L0B2U1kkqXh*";
@@ -35,18 +36,19 @@ $dbname = "alqadiou_homework3";
   
   $sql = "SELECT * FROM school";
     $result = $conn->query($sql);
+    
 if ($result->num_rows > 0) {
-  echo "<table><tr><th>School ID</th><th>School Name</th></tr>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row["school_id"]."</td><td>".$row["school_name"]."</td></tr>";
+?>
+  <tr>
+    <td><?=$row["school_id"]?></td>
+    <td><?=$row["school_name"]?></td>
+  </tr>
+<?php
   }
-  echo "</table>";
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
-
-</body>
-</html>
