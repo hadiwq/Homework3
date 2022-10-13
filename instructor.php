cf692c5213f18c44c4cdff9dc9f763629e7a4f75
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-12 20:44:16 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -3,21 +3,53 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Instructors</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
-</html>
 
-<html>
+
+
 <body>
-echo Um Bernard
+    <h1></h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Instructor ID</th>
+      <th>Instructor Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "165.227.18.177";
+$username = "alqadiou";
+$password = "1[L0B2U1kkqXh*";
+$dbname = "alqadiou_homework3";
+    
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
+$sql = "SELECT instructor_id, instructor_name from instructor";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["instructor_id"]?></td>
+    <td><a href="instructor-classroom.php?id=<?=$row["instructor_id"]?>"><?=$row["instructor_name"]?></a></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
   
-
-<body>
-<html>
