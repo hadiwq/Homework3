09c41660c1d6670518c3fafebb5c2151de60f03a
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-23 15:30:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -43,8 +43,8 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["school_id"]?></td>
-    <td><a href="instructor-course.php?school_id=<?=$row["school_id"]?>"><?=$row["school_id"]?></a></td>
+    <td><a href="instructor-course.php?school_id=<?=$row["school_id"]?>"><?=$row["school_id"]?></td>
+    <td><?=$row["school_name"]?></td>
     
   </tr>
 <?php
