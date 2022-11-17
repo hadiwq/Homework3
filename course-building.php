48e02eb80f96cf122e1db39d442895f32b555165
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-16 22:15:18 -0600

    Update course-building.php

--- course-building.php
+++ course-building.php
@@ -35,7 +35,7 @@ $dbname = "alqadiou_homework3";
   if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
-    $iiid = $_GET['school_id'];
+    $iiid = $_POST['school_id'];
   
   $sql = "SELECT c.description, b.room_building
 FROM course c JOIN classroom b ON c.school_id = b.school_id
