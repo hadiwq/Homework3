ab25970eb5f27c0d09baa0b8db42fcee008f47b4
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-16 19:38:27 -0600

    Update course-building.php

--- course-building.php
+++ course-building.php
@@ -48,7 +48,7 @@ if ($result->num_rows > 0) {
 while($row = $result->fetch_assoc()) {
   ?>
   <tr>
-    <td><?=$row["course_name"]?></td>
+    <td><?=$row["description"]?></td>
     <td><?=$row["room_building"]?></td>
   </tr>
   <?php
