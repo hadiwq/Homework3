4780f13d3f93689c0854fb3434e8683bdeda0158
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-17 21:54:25 -0600

    Create coursesfiltered.php

---
+++ coursesfiltered.php
@@ -0,0 +1,39 @@
+<?php require_once("header.php"); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Instructor Classroom</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+<body>
+    <h1>Courses</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Course ID</th>
+      <th>Prefix</th>
+      <th>Description</th>
+      <th>School ID</th>
+      <th>Instructor ID</th>
+      <th>Instructor Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "165.227.18.177";
+$username = "alqadiou";
+$password = "1[L0B2U1kkqXh*";
+$dbname = "alqadiou_homework3";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$cid = $_POST['school_id'];
